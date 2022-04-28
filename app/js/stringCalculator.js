7e2297022f163cf1b38e6199adf46d9f54eff6e9
BourgneJeremy <jeremy.bourgne@ynov.com>
2022-04-28 14:50:28 +0200

    Add 'add' method tests

--- app/js/stringCalculator.js
+++ app/js/stringCalculator.js
@@ -1,7 +1,46 @@
 export default class StringCalculator {
     constructor() {}
 
-    add(number) {
-        return number;
+    add(numbers) {
+        if (typeof numbers === 'undefined' || numbers.length == 0)
+            return 0;
+
+        if (typeof numbers === 'string') {
+            // process the values
+            let splitedNumbers = numbers.split(",");
+            let sum = 0;
+
+            splitedNumbers.forEach((item) => {
+                if (item.includes(" ")) throw new Error;
+                item = this.removeDelimiters(item);
+
+                let number = parseInt(item);
+                console.log(number);
+                if (number < 0) 
+                {
+                    throw new Error(`Negatives not allowed. ${item}`);
+                }
+
+                sum += parseInt(item);
+            })
+
+            console.log(sum);
+            return sum;
+        }
+
+        return numbers;
+    }
+
+    removeDelimiters(item) {
+        let delimiters = "\n,\r,\b";
+
+        let delimiterItems = delimiters.split(",");
+
+        delimiterItems.forEach((delimiter) => {
+            if (item.includes(delimiter))
+                item = item.replace(delimiter, '');
+        });
+
+        return item;
     }
 }
--- app/tests/stringCalculator.test.js
+++ app/tests/stringCalculator.test.js
@@ -3,7 +3,37 @@ const { default: StringCalculator } = require("../js/stringCalculator.js");
 let stringCalculator = new StringCalculator();
 
 describe("String calculator tests", () => {
-    test("expect add 1 to be 1", () => {
-        expect(stringCalculator.add(1)).toBe(1);
+    // Numbers
+    describe("Numbers", () => {
+        test("Expect null to be 0", () => {
+            expect(stringCalculator.add()).toBe(0);
+        });
+        test("Expect '' to be 0", () => {
+            expect(stringCalculator.add("")).toBe(0);
+        });
+        test('Expect "1" to be 1', () => {
+            expect(stringCalculator.add("1")).toBe(1);
+        });
+        test('Expect "55,72" to be 127', () => {
+            expect(stringCalculator.add("55,72")).toBe(127);
+        })
+        test('Expect "55,72,22" to be 149', () => {
+            expect(stringCalculator.add("55,72,22")).toBe(149);
+        })
+        test('Expect "1\n,2,3" to be 6', () => {
+            expect(stringCalculator.add("1\n,2,3")).toBe(6);
+        })
+    });
+
+    describe("Errors", () => {
+        test('Expect -46 to be Error not allowed', () => {
+            expect(() => stringCalculator.add("-46")).toThrow(`Negatives not allowed. -46`);
+        })
+    })
+
+    describe("Delimiters", () => {
+        test('Expect "2\\r\\b" to be 10', () => {
+            expect(stringCalculator.removeDelimiters("2\r\b")).toBe("2");
+        });
     })
 });
